190d5a76c19955818ae5072b9b01fa720c19b23a
moondef <stpnsamko@gmail.com>
2018-10-10 21:26:49 +0300

    MVP

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// js
+const buttons = document.querySelectorAll('.buttons button')
+const result = document.getElementById('result')
+
+let res = ''
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.innerText === 'C') {
+      res = ''
+      result.innerText = '0'
+    } else if (button.innerText !== '=') {
+      res += button.innerText
+      result.innerText = res
+    } else {
+      result.innerText = eval(res)
+    }
+  })
+})
+
